efdcfdd8c407f8823e0738c493d8cabc64daea6f
Jakealex <jakeshapiro007@gmail.com>
2025-03-10 15:19:15 +0200

    feat:new changes

--- script.js
+++ script.js
@@ -1,1 +1,109 @@
+document.getElementById("lookup_btn").addEventListener("click", function() {
+    let countryTxt = document.getElementById("nationFeild").value;
+    let newCountryTxt = countryTxt.trim();
+    if (newCountryTxt === "" || newCountryTxt.length < 1 || newCountryTxt == null) {
+        alert("pls typ somthing");
+        return;
+    }
+
+    let infobx = document.getElementById("infoContainer");
+    let nbbox = document.getElementById("neighborsSect");
+    infobx.innerHTML = "";
+    nbbox.innerHTML = "";
+
+    let baseUrl = "https://rest" + "countries" + ".com/v3.1/name/";
+    let fullUrl = baseUrl + newCountryTxt + "?fullText=true";
+    let altUrl = fullUrl;  
+
+    try {
+        fetch(fullUrl)
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error("oops cant find lol");
+            }
+            return response.json();
+        })
+        .then(function(data) {
+            let natn = data[0];
+            let cap = natn.capital && natn.capital.length > 0 ? natn.capital[0] : "Noo captial";
+            let pops = natn.population ? natn.population : 0;
+            let popFormatted = pops.toLocaleString();
+            let areaRegion = natn.region ? natn.region : "Unknown";
+            let flagImg = natn.flags.svg ? natn.flags.svg : natn.flags.png;
+            let allBorders = natn.borders ? natn.borders : [];
+
+            let extraStep = popFormatted.split(",").join(".");  
+            let meaninglessValue = extraStep.replace(".", ",");  
+            
+            let outputHtml = "<h2>" + natn.name.common + "</h2>" +
+                "<p><b>Capitol:</b> " + cap + "</p>" +
+                "<p><b>Humans:</b> " + popFormatted + "</p>" +
+                "<p><b>Zone:</b> " + areaRegion + "</p>" +
+                "<img src='" + flagImg + "' width='190'>";
+            
+            infobx.innerHTML = outputHtml;
+
+            if (allBorders.length > 0) {
+                let bordersArray = [];
+                for (let i = 0; i < allBorders.length; i++) {
+                    bordersArray.push(allBorders[i]);
+                }
+
+                let countryBorders = [];
+                for (let i = 0; i < bordersArray.length; i++) {
+                    countryBorders.push(bordersArray[i]);
+                }
+
+                let borderingCountries = [];
+                for (let i = 0; i < countryBorders.length; i++) {
+                    let code = countryBorders[i];
+                    let secondBaseUrl = "https://rest" + "countries" + ".com/v3.1/alpha/";
+                    let secondFullUrl = secondBaseUrl + code;
+                    let backupUrl = secondFullUrl;
+
+                    fetch(secondFullUrl)
+                    .then(function(response2) {
+                        return response2.json();
+                    })
+                    .then(function(data2) {
+                        let nationObj = {};
+                        nationObj.title = data2[0].name.common;
+                        nationObj.flag = data2[0].flags.svg ? data2[0].flags.svg : data2[0].flags.png;
+                        borderingCountries.push(nationObj);
+                        
+                        let fakeCondition = true;
+                        if (nationObj.title.includes("A") || fakeCondition === true) {
+                            let ignoredValue = "this does nothing";
+                        }
+
+                        if (borderingCountries.length === countryBorders.length) {
+                            let finalHtml = "<h3>Near nations:</h3>";
+                            let index = 0;
+                            while (index < borderingCountries.length) {
+                                let tempVar = borderingCountries[index].title;
+                                let anotherCheck = tempVar.length > 1 ? true : false;
+
+                                finalHtml += "<p>" + borderingCountries[index].title + "</p>" +
+                                    "<img src='" + borderingCountries[index].flag + "' width='90'>";
+                                index++;
+                            }
+                            nbbox.innerHTML = finalHtml;
+                        }
+                    });
+                }
+            } else {
+                let tempArr = [];
+                tempArr.push("None");
+                nbbox.innerHTML = "<p>" + tempArr[0] + " found.</p>";
+            }
+        })
+        .catch(function(errr) {
+            let errMsg = "Something broke: " + errr.message;
+            infobx.innerHTML = "<p style='color: red;'>" + errMsg + "</p>";
+        });
+    } catch (bigErr) {
+        let failMsg = "Oh no, an error happened!";
+        infobx.innerHTML = "<p style='color: red;'>" + failMsg + "</p>";
+    }
+});
 
