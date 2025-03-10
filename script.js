40be68feba48a0e07673968abf65bc69503496be
Jakealex <131644736+Jakealex@users.noreply.github.com>
2025-03-10 15:29:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,109 +1,72 @@
-document.getElementById("lookup_btn").addEventListener("click", function() {
-    let countryTxt = document.getElementById("nationFeild").value;
-    let newCountryTxt = countryTxt.trim();
-    if (newCountryTxt === "" || newCountryTxt.length < 1 || newCountryTxt == null) {
-        alert("pls typ somthing");
+
+document.getElementById("search-button").addEventListener("click", fetchCountryData);
+
+async function fetchCountryData() {
+    const countryName = document.getElementById("country-input").value.trim();
+    if (!countryName) {
+        alert("Please enter a country name.");
         return;
     }
 
-    let infobx = document.getElementById("infoContainer");
-    let nbbox = document.getElementById("neighborsSect");
-    infobx.innerHTML = "";
-    nbbox.innerHTML = "";
+    const countryInfoSection = document.getElementById("country-info");
+    const borderingCountriesSection = document.getElementById("bordering-countries");
 
-    let baseUrl = "https://rest" + "countries" + ".com/v3.1/name/";
-    let fullUrl = baseUrl + newCountryTxt + "?fullText=true";
-    let altUrl = fullUrl;  
+    // Clear previous results
+    countryInfoSection.innerHTML = "";
+    borderingCountriesSection.innerHTML = "";
 
     try {
-        fetch(fullUrl)
-        .then(function(response) {
-            if (!response.ok) {
-                throw new Error("oops cant find lol");
-            }
-            return response.json();
-        })
-        .then(function(data) {
-            let natn = data[0];
-            let cap = natn.capital && natn.capital.length > 0 ? natn.capital[0] : "Noo captial";
-            let pops = natn.population ? natn.population : 0;
-            let popFormatted = pops.toLocaleString();
-            let areaRegion = natn.region ? natn.region : "Unknown";
-            let flagImg = natn.flags.svg ? natn.flags.svg : natn.flags.png;
-            let allBorders = natn.borders ? natn.borders : [];
+        const response = await fetch(https://restcountries.com/v3.1/name/${countryName}?fullText=true);
+        
+        if (!response.ok) {
+            throw new Error("Country not found.");
+        }
 
-            let extraStep = popFormatted.split(",").join(".");  
-            let meaninglessValue = extraStep.replace(".", ",");  
-            
-            let outputHtml = "<h2>" + natn.name.common + "</h2>" +
-                "<p><b>Capitol:</b> " + cap + "</p>" +
-                "<p><b>Humans:</b> " + popFormatted + "</p>" +
-                "<p><b>Zone:</b> " + areaRegion + "</p>" +
-                "<img src='" + flagImg + "' width='190'>";
-            
-            infobx.innerHTML = outputHtml;
+        const countryData = await response.json();
+        const country = countryData[0];
 
-            if (allBorders.length > 0) {
-                let bordersArray = [];
-                for (let i = 0; i < allBorders.length; i++) {
-                    bordersArray.push(allBorders[i]);
-                }
+        // Extract required data
+        const capital = country.capital ? country.capital[0] : "No capital available";
+        const population = country.population.toLocaleString();
+        const region = country.region;
+        const flagUrl = country.flags.svg || country.flags.png;
+        const borders = country.borders || [];
 
-                let countryBorders = [];
-                for (let i = 0; i < bordersArray.length; i++) {
-                    countryBorders.push(bordersArray[i]);
-                }
+        // Display country info
+        countryInfoSection.innerHTML = `
+            <h2>${country.name.common}</h2>
+            <p><strong>Capital:</strong> ${capital}</p>
+            <p><strong>Population:</strong> ${population}</p>
+            <p><strong>Region:</strong> ${region}</p>
+            <img src="${flagUrl}" alt="Flag of ${country.name.common}" width="200">
+        `;
 
-                let borderingCountries = [];
-                for (let i = 0; i < countryBorders.length; i++) {
-                    let code = countryBorders[i];
-                    let secondBaseUrl = "https://rest" + "countries" + ".com/v3.1/alpha/";
-                    let secondFullUrl = secondBaseUrl + code;
-                    let backupUrl = secondFullUrl;
+        // Fetch and display bordering countries
+        if (borders.length > 0) {
+            const borderPromises = borders.map(code =>
+                fetch(https://restcountries.com/v3.1/alpha/${code})
+                    .then(res => res.json())
+                    .then(data => ({
+                        name: data[0].name.common,
+                        flag: data[0].flags.svg || data[0].flags.png
+                    }))
+            );
 
-                    fetch(secondFullUrl)
-                    .then(function(response2) {
-                        return response2.json();
-                    })
-                    .then(function(data2) {
-                        let nationObj = {};
-                        nationObj.title = data2[0].name.common;
-                        nationObj.flag = data2[0].flags.svg ? data2[0].flags.svg : data2[0].flags.png;
-                        borderingCountries.push(nationObj);
-                        
-                        let fakeCondition = true;
-                        if (nationObj.title.includes("A") || fakeCondition === true) {
-                            let ignoredValue = "this does nothing";
-                        }
+            const borderingCountries = await Promise.all(borderPromises);
 
-                        if (borderingCountries.length === countryBorders.length) {
-                            let finalHtml = "<h3>Near nations:</h3>";
-                            let index = 0;
-                            while (index < borderingCountries.length) {
-                                let tempVar = borderingCountries[index].title;
-                                let anotherCheck = tempVar.length > 1 ? true : false;
+            borderingCountriesSection.innerHTML = <h3>Bordering Countries:</h3>;
+            borderingCountries.forEach(country => {
+                borderingCountriesSection.innerHTML += `
+                    <p>${country.name}</p>
+                    <img src="${country.flag}" alt="Flag of ${country.name}" width="100">
+                `;
+            });
+        } else {
+            borderingCountriesSection.innerHTML = "<p>No bordering countries.</p>";
+        }
 
-                                finalHtml += "<p>" + borderingCountries[index].title + "</p>" +
-                                    "<img src='" + borderingCountries[index].flag + "' width='90'>";
-                                index++;
-                            }
-                            nbbox.innerHTML = finalHtml;
-                        }
-                    });
-                }
-            } else {
-                let tempArr = [];
-                tempArr.push("None");
-                nbbox.innerHTML = "<p>" + tempArr[0] + " found.</p>";
-            }
-        })
-        .catch(function(errr) {
-            let errMsg = "Something broke: " + errr.message;
-            infobx.innerHTML = "<p style='color: red;'>" + errMsg + "</p>";
-        });
-    } catch (bigErr) {
-        let failMsg = "Oh no, an error happened!";
-        infobx.innerHTML = "<p style='color: red;'>" + failMsg + "</p>";
+    } catch (error) {
+        countryInfoSection.innerHTML = <p style="color: red;">Error: ${error.message}</p>;
     }
-});
+}
 
